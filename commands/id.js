ffcb2482c4c43b63ec5e3cbdd12a4a47b0e2fc2b
a201a <115426699+a201a@users.noreply.github.com>
2024-02-23 06:18:16 +0000

    add id

---
+++ commands/id.js
@@ -0,0 +1,41 @@
+module.exports = (bot) => {
+    bot.onText(/\/ايدي/, async (msg) => {
+        const chatId = msg.chat.id;
+        const userId = msg.from.id;
+        
+        try {
+            const userInfo = await bot.getChat(userId);
+            const firstName = userInfo.first_name;
+            const lastName = userInfo.last_name ? userInfo.last_name : '';
+            const username = userInfo.username ? `(@${userInfo.username})` : '';
+            const userDesc = `المستخدم: ${firstName} ${lastName}\nالمعرّف: ${username}`;
+
+            const bio = userInfo.bio ? `\nالنبذة الشخصية: ${userInfo.bio}` : '';
+            const userDescWithBio = `${userDesc}${bio}`;
+
+            let status = '';
+            if (userInfo.is_admin) {
+                status = 'مشرف';
+            } else {
+                const chatMember = await bot.getChatMember(chatId, userId);
+                if (chatMember.status === 'creator') {
+                    status = 'المالك';
+                } else {
+                    status = 'عضو';
+                }
+            }
+            const userDescWithStatus = `${userDescWithBio}\nالحالة: ${status}`;
+
+            const photo = await bot.getUserProfilePhotos(userId, 0, 1);
+            if (photo.total_count > 0) {
+                const fileId = photo.photos[0][0].file_id;
+                bot.sendPhoto(chatId, fileId, { caption: userDescWithStatus });
+            } else {
+                bot.sendMessage(chatId, "لا يوجد صورة للمستخدم.\n" + userDescWithStatus);
+            }
+        } catch (error) {
+            console.error("Error fetching user photo:", error);
+            bot.sendMessage(chatId, "حدث خطأ أثناء جلب صورة المستخدم.");
+        }
+    });
+};
