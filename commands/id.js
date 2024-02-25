ba5b8a8970019705f59cbdf1efbe83d437e06469
a201a <115426699+a201a@users.noreply.github.com>
2024-02-25 18:07:12 +0000

    update id

--- commands/id.js
+++ commands/id.js
@@ -1,19 +1,17 @@
 module.exports = (bot) => {
-    bot.onText(/\/ايدي/, async (msg) => {
+    bot.onText(/\/getinfo/, async (msg) => {
         const chatId = msg.chat.id;
         const userId = msg.from.id;
-        
+
         try {
             let targetUserId = userId;
             if (msg.reply_to_message && msg.reply_to_message.from) {
                 targetUserId = msg.reply_to_message.from.id;
             }
-            
+
             const userInfo = await bot.getChat(targetUserId);
-            const firstName = userInfo.first_name;
-            const lastName = userInfo.last_name ? userInfo.last_name : '';
-            const username = userInfo.username ? `(@${userInfo.username})` : '';
-            const userDesc = `المستخدم: ${firstName} ${lastName}\nالمعرّف: ${username}`;
+            const username = userInfo.username ? userInfo.username : '';
+            const userDesc = `المستخدم: ${userInfo.first_name} ${userInfo.last_name ? userInfo.last_name : ''}`;
 
             const bio = userInfo.bio ? `\nالنبذة الشخصية: ${userInfo.bio}` : '';
             const userDescWithBio = `${userDesc}${bio}`;
@@ -31,17 +29,23 @@ module.exports = (bot) => {
             }
             const userDescWithStatus = `${userDescWithBio}\nالحالة: ${status}`;
 
+            const replyMarkup = JSON.stringify({
+                inline_keyboard: [
+                    [{ text: username, url: `https://t.me/${username}` }]
+                ]
+            });
+
             const photo = await bot.getUserProfilePhotos(targetUserId, 0, 1);
             if (photo.total_count > 0) {
                 const fileId = photo.photos[0][0].file_id;
-                bot.sendPhoto(chatId, fileId, { caption: userDescWithStatus });
+                bot.sendPhoto(chatId, fileId, { caption: userDescWithStatus, reply_markup: replyMarkup });
             } else {
-                bot.sendMessage(chatId, "لا يوجد صورة للمستخدم.\n" + userDescWithStatus);
+                bot.sendMessage(chatId, userDescWithStatus, { reply_markup: replyMarkup });
             }
             
         } catch (error) {
-            console.error("Error fetching user photo:", error);
-            bot.sendMessage(chatId, "حدث خطأ أثناء جلب صورة المستخدم.");
+            console.error("Error fetching user info:", error);
+            bot.sendMessage(chatId, "حدث خطأ أثناء جلب معلومات المستخدم.");
         }
     });
 };
