bd5c9c8f23dc74a4e3728dc8c72ce3f1a6686e60
a201a <115426699+a201a@users.noreply.github.com>
2024-02-27 08:29:39 +0000

    updated groupinfo

--- commands/groupinfo.js
+++ commands/groupinfo.js
@@ -1,24 +1,28 @@
-// groupInfoCommand.js
 module.exports = (bot) => {
     bot.onText(/\/groupinfo/, (msg) => {
         const chatId = msg.chat.id;
 
         bot.getChat(chatId).then((chat) => {
-            bot.getChatMembersCount(chatId).then((count) => {
+            Promise.all([
+                bot.getChatMemberCount(chatId),
+                bot.getChatAdministrators(chatId)
+            ]).then(([count, admins]) => {
                 const groupInfo = `
                 معلومات المجموعة:
                 - اسم المجموعة: ${chat.title}
+                - وصف المجموعة: 
+${chat.description || 'لا يوجد'}
                 - معرف المجموعة: ${chat.id}
                 - نوع المجموعة: ${chat.type}
                 - عدد الأعضاء: ${count}
+                - عدد المشرفين: ${admins.length}
                 - يمكن للأعضاء دعوة الآخرين: ${chat.can_invite_users ? 'نعم' : 'لا'}
                 - معرف المالك: ${chat.owner ? chat.owner.id : 'غير معروف'}
                 - الرابط الدائم للمجموعة: ${chat.invite_link ? chat.invite_link : 'غير متاح'}
                 `;
-
                 bot.sendMessage(chatId, groupInfo);
             }).catch((error) => {
-                console.error('Error getting group members count:', error);
+                console.error('Error getting group info:', error);
             });
         }).catch((error) => {
             console.error('Error getting group info:', error);
