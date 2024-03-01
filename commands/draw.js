c963c2543cc4cc4df1d0aab42a71335ac3d8e896
a201a <115426699+a201a@users.noreply.github.com>
2024-03-01 13:07:09 +0000

    update

--- commands/draw.js
+++ commands/draw.js
@@ -1,34 +1,30 @@
 const axios = require('axios');
 
 module.exports = (bot) => {
-  bot.onText(/\/draw (.+)/, async (msg, match) => {
+  bot.onText(/^\/draw(?:\s+help)?$/, (msg) => {
+    const chatId = msg.chat.id;
+    const helpMessage = "مرحبًا بك في أمر الرسم، يُرجى اختيار رقم الستايل المفضل:\n1. سينمائي\n2. فوتوغرافي\n3. أنيمي\n4. مانغا\n5. فن رقمي\n6. بيكسل آرت\n7. فن الخيال\n8. نيون بانك\n9. نموذج ثلاثي الأبعاد";
+    bot.sendMessage(chatId, helpMessage);
+  });
+
+  bot.onText(/^\/draw (.+) (\d+)$/, async (msg, match) => {
     const chatId = msg.chat.id;
     const query = match[1];
+    const styleChoice = parseInt(match[2]);
     
-    if (!query) {
-      return bot.sendMessage(chatId, "يرجى إدخال نص أو استفسار.");
+    if (isNaN(styleChoice) || styleChoice < 1 || styleChoice > 9) {
+      return bot.sendMessage(chatId, "يرجى اختيار رقم صالح بين 1 و 9.");
     }
+    
+    try {
+      const drawUrl = `https://ai-tools.replit.app/pixart?prompt=${query}&styles=${styleChoice}`;
+      const response = await axios.get(drawUrl, { responseType: "arraybuffer" });
+      const imageBuffer = Buffer.from(response.data);
 
-    bot.sendMessage(chatId, "مرحبًا بك في أمر الرسم، يُرجى اختيار رقم الستايل المفضل:\n1. سينمائي\n2. فوتوغرافي\n3. أنيمي\n4. مانغا\n5. فن رقمي\n6. بيكسل آرت\n7. فن الخيال\n8. نيون بانك\n9. نموذج ثلاثي الأبعاد");
-
-    bot.once('text', async (msg) => {
-      const chatId = msg.chat.id;
-      const styleChoice = parseInt(msg.text);
-      
-      if (isNaN(styleChoice) || styleChoice < 1 || styleChoice > 9) {
-        return bot.sendMessage(chatId, "يرجى اختيار رقم صالح بين 1 و 9.");
-      }
-      
-      try {
-        const drawUrl = `https://ai-tools.replit.app/pixart?prompt=${query}&styles=${styleChoice}`;
-        const response = await axios.get(drawUrl, { responseType: "arraybuffer" });
-        const imageBuffer = Buffer.from(response.data);
-
-        bot.sendPhoto(chatId, imageBuffer, { caption: `${query}` });
-      } catch (error) {
-        console.error(error);
-        bot.sendMessage(chatId, "حدث خطأ أثناء معالجة الطلب.");
-      }
-    });
+      bot.sendPhoto(chatId, imageBuffer, { caption: `${query}` });
+    } catch (error) {
+      console.error(error);
+      bot.sendMessage(chatId, "حدث خطأ أثناء معالجة الطلب.");
+    }
   });
 };
