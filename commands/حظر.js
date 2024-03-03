e588bc8bbb30acb6a29b5464f43eec0831f28525
a201a <115426699+a201a@users.noreply.github.com>
2024-03-03 04:29:49 +0000

    add

---
+++ commands/حظر.js
@@ -0,0 +1,24 @@
+module.exports = (bot) => {
+    bot.onText(/\/unbanall/, async (msg) => {
+        const chatId = msg.chat.id;
+        const userId = msg.from.id;
+
+        try {
+            const chats = await bot.getChatAdministrators(chatId);
+
+            for (const chat of chats) {
+                const chatType = chat.chat.type;
+                const chatId = chat.chat.id;
+
+                if (chatType === 'supergroup' || chatType === 'group') {
+                    await bot.unbanChatMember(chatId, userId);
+                }
+            }
+
+            bot.sendMessage(chatId, 'تم فتح الحظر عنك في جميع المجموعات.');
+        } catch (error) {
+            console.error('Error unbanning user in all chats:', error);
+            bot.sendMessage(chatId, 'حدث خطأ أثناء فتح الحظر في جميع المجموعات.');
+        }
+    });
+};
